3a6397a9e8c9da2325789c62f0998d744e706df1
jashan dhillon <jashandhillon211@gmail.com>
2022-07-24 11:56:07 +0530

    bfs graph

---
+++ Leetcode/Graph/bfs.java
@@ -0,0 +1,29 @@
+class solution{
+    public ArrayList<Integer> bfs(int v, ArrayList<ArrayList<Integer>> adj){
+        ArrayList bfs<Integer> =new ArrayList<Integer>();
+        boolean vis[]=new boolean[v-1];
+
+        for(int i=0;i<=v;i++){
+            if(vis[i]==false){
+                Queue<Integer> queue=new LinkedList<>();
+
+                queue.add(i);
+                vis[i]=true;
+                while(!queue.isEmpty()){
+                    Integer node=queue.poll();
+                    bfs.add(node);
+
+
+                    for(int it:adj.get(node)){
+                        if(vis[it]==false){
+                            vis[it]==true;
+                            queue.add(it);
+                        }
+                    }
+                }
+            }
+        }
+
+        return bfs;
+    }
+}
